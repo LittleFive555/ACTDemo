9d6370fb271e877199fb199051dfa2f215b015cb
MengZhangxi <liulimengplay@163.com>
2023-02-09 12:10:50 +0800

    添加InputHandler，用于处理输入，为不同输入设备做准备（键盘、手柄、虚拟按键等）

---
+++ Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class InputHandler
+{
+    public static bool GetInput(Operation operation)
+    {
+        return Input.GetKey(operation.TriggerKey);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,9 +31,9 @@ public class PlayerController : CharacterController
     {
         // Move
         float speed = 0;
-        if (Input.GetKey(OperationBinder.Instance.LeftMove.TriggerKey))
+        if (InputHandler.GetInput(OperationBinder.Instance.LeftMove))
             speed -= _character.MoveSpeed;
-        if (Input.GetKey(OperationBinder.Instance.RightMove.TriggerKey))
+        if (InputHandler.GetInput(OperationBinder.Instance.RightMove))
             speed += _character.MoveSpeed;
         float step = Time.deltaTime * speed;
 
@@ -46,14 +46,14 @@ public class PlayerController : CharacterController
         transform.position += Vector3.right * step;
 
         // Jump
-        if (Input.GetKey(OperationBinder.Instance.Jump.TriggerKey) && !_isJumping)
+        if (InputHandler.GetInput(OperationBinder.Instance.Jump) && !_isJumping)
         {
             _isJumping = true;
             _rigidbody2D.velocity = Vector2.up * _character.JumpVelocity;
         }
 
         // Attack
-        if (Input.GetKeyDown(OperationBinder.Instance.Attack.TriggerKey))
+        if (InputHandler.GetInput(OperationBinder.Instance.Attack))
         {
             _animator.SetTrigger("Attack");
         }
