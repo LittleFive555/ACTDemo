b6c0f40a6c41c76c6f54ac9e83e2c66d2810c072
MengZhangxi <liulimengplay@163.com>
2023-03-20 17:06:29 +0800

    将Character类单独提为一个脚本

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -22,19 +22,3 @@ public class CharacterController : MonoBehaviour
             target.GetComponent<CharacterController>().GetHurt();
     }
 }
-
-public class Character
-{
-    public int Health { get; private set; }
-
-    public float MoveSpeed { get; private set; }
-
-    public float JumpVelocity { get; private set; }
-
-    public Character(int health, float maxMoveSpeed, float jumpVelocity)
-    {
-        Health = health;
-        MoveSpeed = maxMoveSpeed;
-        JumpVelocity = jumpVelocity;
-    }
-}
---
+++ Assets/Scripts/Model/Character.cs
@@ -0,0 +1,15 @@
+﻿public class Character
+{
+    public int Health { get; private set; }
+
+    public float MoveSpeed { get; private set; }
+
+    public float JumpVelocity { get; private set; }
+
+    public Character(int health, float maxMoveSpeed, float jumpVelocity)
+    {
+        Health = health;
+        MoveSpeed = maxMoveSpeed;
+        JumpVelocity = jumpVelocity;
+    }
+}
